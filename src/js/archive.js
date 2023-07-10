0ee33260e218936f0744335b82480923dfeb80f4
igorchagov <igorchagov@mail.ru>
2023-07-10 18:03:48 +0300

    fixed contacts offset

--- src/js/archive.js
+++ src/js/archive.js
@@ -1,33 +1,40 @@
 let archiveContainer = document.querySelector('.archive__container');
 
 window.addEventListener('load', async function () {
-  let url = './files/archive.json'
+  let url = 'https://www.itsrussiaforum.ru/api/archive.json'
   let response = await fetch(url)
   let json = await response.json();
   let yearsArr = [] //Массив под года 
+
+
   for (let i = 0; i < json.length; i++) {
     yearsArr.push(json[i].YEAR)
-  } //Добавляем все года в массив
-  yearsArr = [...new Set(yearsArr)]; // Избавляемся от повторяющихся чисел 
+    yearsArr = [...new Set(yearsArr)]; // Избавляемся от повторяющихся чисел 
 
-  yearsArr.forEach(el => {
+
+    let a = document.createElement('a'); //Создаем DOM
+    a.href = json[i].LINK;  //Добавляем ссылку
+    a.className = 'archive__item'; // Добавляем класс
+    a.textContent = json[i].NAME; // Добавляем контент 
+    
+  }
+
+
+  for (let i = 0; i < yearsArr.length; i++) {
     let div = document.createElement('div');
-    div.className = `archive__box archive__box_${el}`;
-    archiveContainer.appendChild(div)
-  }) //создаем контейнеры для ссылок
+    div.className = `archive__box`;
+    div.dataset.year = yearsArr[i];
+    archiveContainer.appendChild(div)//создаем контейнеры для ссылок
 
-  let archiveBox = document.querySelectorAll('.archive__box');
-
-  for (let i = 0; i < archiveBox.length; i++) {
     for (let j = 0; j < json.length; j++) {
-      if (archiveBox[i].classList[1].slice(-4) == json[j].YEAR) {
-        console.log(json[j].YEAR)
+      if (div.dataset.year == json[j].YEAR) {
         let a = document.createElement('a');
         a.href = json[j].LINK;
         a.className = 'archive__item'
         a.textContent = json[j].NAME;
-        archiveBox[i].appendChild(a)
+        div.appendChild(a)
       }
     }
-  }// Добавляем ссылки по году
+  }
 })
+
