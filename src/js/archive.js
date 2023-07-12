a638740312027a4762eaa7ad0f8b0ebe7cdd99ee
igorchagov <igorchagov@mail.ru>
2023-07-12 18:15:33 +0300

    change script for archive

--- src/js/archive.js
+++ src/js/archive.js
@@ -1,24 +1,19 @@
 let archiveContainer = document.querySelector('.archive__container');
 
+
+
 window.addEventListener('load', async function () {
   let url = 'https://www.itsrussiaforum.ru/api/archive.json'
   let response = await fetch(url)
   let json = await response.json();
   let yearsArr = [] //Массив под года 
 
-
+ 
   for (let i = 0; i < json.length; i++) {
     yearsArr.push(json[i].YEAR)
     yearsArr = [...new Set(yearsArr)]; // Избавляемся от повторяющихся чисел 
 
-
-    let a = document.createElement('a'); //Создаем DOM
-    a.href = json[i].LINK;  //Добавляем ссылку
-    a.className = 'archive__item'; // Добавляем класс
-    a.textContent = json[i].NAME; // Добавляем контент 
-    
   }
-
 
   for (let i = 0; i < yearsArr.length; i++) {
     let div = document.createElement('div');
